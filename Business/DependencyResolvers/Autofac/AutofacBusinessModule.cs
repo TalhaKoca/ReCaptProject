e7f57a22c251b85dcca90206be3414c1436c88bc
Talha KOCA <mertyigit306@gmail.com>
2021-03-04 10:46:02 +0300

    04.03.2021 Autofac

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule: Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
+            // biri senden ICarService isterse Ona ICarManager örneği ver anlamına geliyor.
+            builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
+
+        }
+    }
+}
